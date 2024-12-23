cd9570b641455f57a74699475c5adccf0027e877
maxim <pronet41@gmail.com>
2024-12-23 13:49:18 +0200

    Added a Delete group option

--- src/main/java/com/example/lyricswebapp/repository/WordRepository.java
+++ src/main/java/com/example/lyricswebapp/repository/WordRepository.java
@@ -1,6 +1,5 @@
 package com.example.lyricswebapp.repository;
 
-import com.example.lyricswebapp.model.SongModel;
 import com.example.lyricswebapp.model.WordModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/example/lyricswebapp/service/PhraseService.java
+++ src/main/java/com/example/lyricswebapp/service/PhraseService.java
@@ -26,6 +26,7 @@ public class PhraseService {
         this.songRepository = songRepository;
     }
 
+    @Transactional
     public PhraseModel createUserDefinedPhrase(PhraseRequestDTO request) {
         // Find the song by ID
         SongModel song = songRepository.findById(request.getSongId())
